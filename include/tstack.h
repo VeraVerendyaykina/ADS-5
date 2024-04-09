804a57ff72784b5b17b1efabba66173759bd01c5
VeraVerendyaykina <151723475+VeraVerendyaykina@users.noreply.github.com>
2024-04-09 18:42:13 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,43 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+  private:
+    T *data;
+    int top;
+
+ public:
+    TStack() : top(-1) {
+        data = new T[size];
+    }
+
+    ~TStack() {
+        delete[] data;
+    }
+
+    void push(T value) {
+        if (top == size - 1) {
+            throw std::string("Overflow");
+        }
+        data[++top] = value;
+    }
+
+    T pop() {
+        if (top == -1) {
+            throw std::string("Underflow");
+        }
+        return data[top--];
+    }
+
+    bool isEmpty() {
+        return top == -1;
+    }
+
+    T watch() {
+        if (top < 0) {
+            throw std::string("Underflow");
+        }
+        return data[top];
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
